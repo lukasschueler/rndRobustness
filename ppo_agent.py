5c273996dcb99fe57778a9266261b718c393b563
lukasschueler <lukas@newconnexxions.de>
2021-08-08 21:46:26 +0200

    tracing

--- ppo_agent.py
+++ ppo_agent.py
@@ -417,9 +417,7 @@ class PpoAgent(object):
         """
         if self.I.step_count == 0: # On the zeroth step with a new venv, we need to call reset on the environment
             print("IT REALLY HAPPENEND")
-            print("THIS IS I:", self.I)
-            print("THIS IS I.venvs:", self.I.venvs)
-            print("THIS IS I.venvs[l]:", self.I.venvs[l])
+            print("THIS IS I:", dir(self.I))
             
             # ob = self.I.venvs[l].reset()
             # out = self.I.env_results[l] = (ob, None, np.ones(self.I.lump_stride, bool), {})
