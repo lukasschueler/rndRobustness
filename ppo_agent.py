b8b124c689ec04e4bf6c993fcbee123d72b59009
lukasschueler <lukas@newconnexxions.de>
2021-08-08 22:14:05 +0200

    tracing

--- ppo_agent.py
+++ ppo_agent.py
@@ -416,11 +416,8 @@ class PpoAgent(object):
         Using step_wait if necessary
         """
         if self.I.step_count == 0: # On the zeroth step with a new venv, we need to call reset on the environment
-            print("IT REALLY HAPPENEND")
-            print("THIS IS I:", dir(self.I))
-            
-            # ob = self.I.venvs[l].reset()
-            # out = self.I.env_results[l] = (ob, None, np.ones(self.I.lump_stride, bool), {})
+            ob = self.I.venvs[l].reset()
+            out = self.I.env_results[l] = (ob, None, np.ones(self.I.lump_stride, bool), {})
         else:
             if self.I.env_results[l] is None:
                 out = self.I.env_results[l] = self.I.venvs[l].step_wait()
